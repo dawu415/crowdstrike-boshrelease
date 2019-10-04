ea97ffeca4ae3a7c5c8ec31aee9ff86506a13ff8
David Wu <david.wu.415@gmail.com>
2019-10-04 10:46:52 -0500

    Added Add blob script.sh

---
+++ add-crowdstrike-agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set +eux 
+bosh init-release
+
+filename=$(basename $1)
+version=$(echo $filename | sed -e 's/[a-zA-Z-]*_//' -e 's/_.*//')
+bosh add-blob $1 ${filename}
+
+bosh int ./packages/crowdstrike-agent/spec \
+     --var=crowdstrike_deb_file=${filename} > ./install/tmp
+
+mv ./install/tmp ./packages/crowdstrike-agent/spec
+bosh int runtime-config/crowdstrike.yml \
+     --var=crowdstrike_version=${version} \
+     --var=customer_id=$2 > generated_runtime_config.yml
